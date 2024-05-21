ba94a95d7f000055d0a2d128c0b1649545721b1c
kitngapala <guianangian04@gmail.com>
2024-05-21 10:27:04 +0800

    Create 00shoti.js

---
+++ modules/commands/00shoti.js
@@ -0,0 +1,56 @@
+module.exports.config = {
+  name: "shoti",
+  version: "1.0.0",
+  credits: "shoti-api",
+  description: "Generate random tiktok girl videos",
+  hasPermssion: 0,
+  commandCategory: "other",
+  usage: "[shoti]",
+  cooldowns: 5,
+  dependencies: [],
+  usePrefix: true,
+};
+
+module.exports.run = async function ({ api, event }) {
+  try {
+    const axios = require("axios");
+    const request = require("request");
+    const fs = require("fs");
+    let response = await axios.post(
+      "https://shoti-srv1.onrender.com/api/v1/get",
+      {
+        apikey: "YOUR-API-KEY",
+      },
+    );
+    if (response.data.code !== 200) {
+      api.sendMessage(
+        `API ERROR: ${response.data}`,
+        event.threadID,
+        event.messageID,
+      );
+      return;
+    }
+    var file = fs.createWriteStream(__dirname + "/cache/shoti.mp4");
+    var rqs = request(encodeURI(response.data.data.url));
+    rqs.pipe(file);
+    file.on("finish", () => {
+      return api.sendMessage(
+        {
+          body: `@${response.data.data.user.username}`,
+          attachment: fs.createReadStream(__dirname + "/cache/shoti.mp4"),
+        },
+        event.threadID,
+        event.messageID,
+      );
+    });
+    file.on("error", (err) => {
+      api.sendMessage(`Shoti Error: ${err}`, event.threadID, event.messageID);
+    });
+  } catch (error) {
+    api.sendMessage(
+      "An error occurred while generating video:" + error,
+      event.threadID,
+      event.messageID,
+    );
+  }
+};
